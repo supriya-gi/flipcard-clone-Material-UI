1ec8a4a7a92eaadffa3de3752db0845bcdb557de
Supriya Punekar <74414460+supriya-gi@users.noreply.github.com>
2022-11-12 10:33:21 +0530

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,7 +21,13 @@ function Footer() {
       <Box>
         <Container maxWidth="lg">
           <Grid container paddingTop={"10px"} paddingBottom={"10px"}>
-            <Grid item xs={12} sm={2} sx={{ marginTop: { xs: 2, sm: 2 } }}>
+            <Grid
+              item
+              xs={12}
+              sm={3}
+              md={2}
+              sx={{ marginTop: { xs: 2, sm: 2 } }}
+            >
               <Box className="heading">ABOUT</Box>
               <Box style={{ textAlign: "left" }}>Contact Us</Box>
               <Box style={{ textAlign: "left" }}>About Us</Box>
@@ -31,7 +37,13 @@ function Footer() {
               <Box style={{ textAlign: "left" }}>Flipkart Wholesale</Box>
               <Box style={{ textAlign: "left" }}>Corporate </Box>
             </Grid>
-            <Grid item xs={12} sm={1} sx={{ marginTop: { xs: 2, sm: 2 } }}>
+            <Grid
+              item
+              xs={12}
+              sm={3}
+              md={2}
+              sx={{ marginTop: { xs: 2, sm: 2 } }}
+            >
               <Box className="heading">HELP</Box>
               <Box style={{ textAlign: "left" }}>Payments</Box>
               <Box style={{ textAlign: "left" }}>Shipping</Box>
@@ -39,7 +51,13 @@ function Footer() {
               <Box style={{ textAlign: "left" }}>FAQ</Box>
               <Box style={{ textAlign: "left" }}>Report Infringement</Box>
             </Grid>
-            <Grid item xs={12} sm={1} sx={{ marginTop: { xs: 2, sm: 2 } }}>
+            <Grid
+              item
+              xs={12}
+              sm={3}
+              md={2}
+              sx={{ marginTop: { xs: 2, sm: 2 } }}
+            >
               <Box className="heading">POLICY</Box>
               <Box style={{ textAlign: "left" }}>Return Policy</Box>
               <Box style={{ textAlign: "left" }}>Terms Of Use</Box>
@@ -48,7 +66,13 @@ function Footer() {
               <Box style={{ textAlign: "left" }}>Sitemap</Box>
               <Box style={{ textAlign: "left" }}>EPR Compliance</Box>
             </Grid>
-            <Grid item xs={12} sm={1} sx={{ marginTop: { xs: 2, sm: 2 } }}>
+            <Grid
+              item
+              xs={12}
+              sm={3}
+              md={1}
+              sx={{ marginTop: { xs: 2, sm: 2 } }}
+            >
               <Box className="heading">SOCIAL</Box>
               <Box style={{ textAlign: "left" }}>Facebook</Box>
               <Box style={{ textAlign: "left" }}>Twitter</Box>
@@ -59,7 +83,8 @@ function Footer() {
               className="list"
               item
               xs={12}
-              sm={3}
+              sm={4}
+              md={3}
               sx={{ marginTop: { xs: 2, sm: 2 } }}
             >
               <Box className="heading">Mail Us</Box>
@@ -69,7 +94,13 @@ function Footer() {
                 Village, Bengaluru, 560103, Karnataka, India
               </Box>
             </Grid>
-            <Grid item xs={12} sm={3} sx={{ marginTop: { xs: 2, sm: 2 } }}>
+            <Grid
+              item
+              xs={12}
+              sm={4}
+              md={3}
+              sx={{ marginTop: { xs: 2, sm: 2 } }}
+            >
               <Box className="heading">Registered Office Address:</Box>
               <Box style={{ textAlign: "left" }}>
                 Flipkart Internet Private Limited, Buildings Alyssa, Begonia &
